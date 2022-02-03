517d286223d5aa1163bd021e87d07e4048b704fd
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-02-04 00:31:54 +0500

    added faq asection

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ let elMenuBox = document.getElementById('menubox');
 let elopenMenuBox = document.getElementById('openMenubox');
 let elcloseMenuBox = document.getElementById('closeMenubox');
 let ellogo = document.getElementById('logo')
+let elfaqmodalopen = document.getElementById('faqModal');
+let elfaqmodal = document.getElementById('faqModalbox');
+let elfaqIcon = document.getElementById('faqIcon');
+let elfaqmodalopenfirst = document.getElementById('faqModalfirst');
+let elfaqmodalfirst = document.getElementById('faqModalboxfirst');
+let elfaqIconfirst = document.getElementById('faqIconfirst');
+let elfaqmodalopensecond = document.getElementById('faqModalsecond');
+let elfaqmodalsecond = document.getElementById('faqModalboxsecond');
+let elfaqIconsecond = document.getElementById('faqIconsecond');
+let elfaqmodalopenthird = document.getElementById('faqModalthird');
+let elfaqmodalthird = document.getElementById('faqModalboxthird');
+let elfaqIconthird = document.getElementById('faqIconthird');
 
 elopenMenuBox.addEventListener('click',function(){
     elMenuBox.classList.add('show');
@@ -12,7 +24,21 @@ elcloseMenuBox.addEventListener('click',function(){
     elMenuBox.classList.remove('show');
     elopenMenuBox.classList.remove('hidebtn')
     ellogo.classList.remove('hidelogo')
-
-
 })
 
+elfaqmodalopen.addEventListener('click',function(){
+    elfaqmodal.classList.toggle('faq-show');
+    elfaqIcon.classList.toggle('rotaticon')
+})
+elfaqmodalopenfirst.addEventListener('click',function(){
+    elfaqmodalfirst.classList.toggle('faq-show');
+    elfaqIconfirst.classList.toggle('rotaticon')
+})
+elfaqmodalopensecond.addEventListener('click',function(){
+    elfaqmodalsecond.classList.toggle('faq-show');
+    elfaqIconsecond.classList.toggle('rotaticon')
+})
+elfaqmodalopenthird.addEventListener('click',function(){
+    elfaqmodalthird.classList.toggle('faq-show');
+    elfaqIconthird.classList.toggle('rotaticon')
+})
