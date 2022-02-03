9ddb22a80672dc3772501f150c69d59fb5a2f747
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-02-03 05:37:29 +0500

    almost redy header

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let elMenuBox = document.getElementById('menubox');
 let elopenMenuBox = document.getElementById('openMenubox');
 let elcloseMenuBox = document.getElementById('closeMenubox');
+let ellogo = document.getElementById('logo')
 
 elopenMenuBox.addEventListener('click',function(){
     elMenuBox.classList.add('show');
+    elopenMenuBox.classList.add('hidebtn')
+    ellogo.classList.add('hidelogo')
 })
 elcloseMenuBox.addEventListener('click',function(){
     elMenuBox.classList.remove('show');
+    elopenMenuBox.classList.remove('hidebtn')
+    ellogo.classList.remove('hidelogo')
+
+
 })
 
